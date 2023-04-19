b522aafed929981f8e949e4979ab0675e86b9b10
luisc <luisc@KB-LAPTOP-2020>
2023-04-20 00:16:52 +0200

    Subir archivos a github A proyecto https://github.com/luisc3615290/Tarea_ED_UT05_NEW.git A fecha 19/04/2023
    Propósito: Subida inicial del repositorio local a GitHub 2

---
+++ src/tarea_ed_ut05/Tarea_ED_UT05.java
@@ -0,0 +1,33 @@
+package tarea_ed_ut05;
+
+/**
+ * @author luisc
+ */
+public class Tarea_ED_UT05 {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+        
+        miCuenta = new CCuenta("Manuel Mantecas","0000-1234-55-123456789",3000,0);
+        try 
+        {
+            miCuenta.retirar(2300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al retirar");
+        }
+        
+        try
+        {
+            System.out.println("Ingreso en cuenta");
+            miCuenta.ingresar(695);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al ingresar");
+        }
+        saldoActual = miCuenta.getEstado();
+        System.out.println("El saldo actual es €"+ saldoActual );
+    }
+    
+}
